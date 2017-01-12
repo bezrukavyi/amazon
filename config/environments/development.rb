1689700729b28f8bda03d4790f37941c1c02e46c
bezrukavyi <yaroslav555@gmail.com>
2017-01-12 11:19:02 +0200

    Install devise and simple form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,14 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = {
+    :address => "localhost",
+    :port => 25,
+    :domain => "gmail.com",
+  }
 
   config.action_mailer.perform_caching = false
 
