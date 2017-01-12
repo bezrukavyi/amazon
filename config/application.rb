45459e0cede18e7bfccfb4361315308cf396a643
bezrukavyi <yaroslav555@gmail.com>
2017-01-12 09:47:29 +0200

    Config for amazon

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Amazon
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework  :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  end
+end
