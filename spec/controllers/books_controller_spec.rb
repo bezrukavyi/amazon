22f1097e2f7550243986608b194d970725262764
bezrukavyi <yaroslav555@gmail.com>
2017-01-18 13:50:07 +0200

    Upgrade test for books_controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -15,6 +15,29 @@ RSpec.describe BooksController, type: :controller do
         expect(response).to render_template(:index)
       end
     end
+
+    context 'with filter' do
+      let(:horror) { create :category }
+      let(:drama) { create :category }
+      let(:book_1) { create :book, category: horror, price: 25.0 }
+      let(:book_2) { create :book, category: horror, price: 50.0 }
+      let(:book_3) { create :book, category: drama, price: 100.0 }
+      let(:book_4) { create :book, category: horror, price: 75.0 }
+
+      it 'set category' do
+        books = [book_1, book_2, book_3, book_4]
+        get :index, params: { with_category: horror.title }
+        expect(assigns(:books).count).to eq(3)
+      end
+
+      it 'set price' do
+        books = [book_1, book_2, book_3, book_4]
+        get :index, params: { sorted_by: :hight_price }
+        expect(assigns(:books)).to eq([book_3, book_4, book_2, book_1])
+      end
+
+    end
+
   end
 
   describe 'GET #show' do
