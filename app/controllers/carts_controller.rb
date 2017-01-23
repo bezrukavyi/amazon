5db34ffaa6c41acbfff777e297983862095d1644
bezrukavyi <yaroslav555@gmail.com>
2017-01-23 17:51:53 +0200

    Added feateure tests for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,9 +8,15 @@ class CartsController < ApplicationController
     @coupon_form = CouponForm.from_params(params[:order][:coupon])
     options = { order: current_order, params: params, coupon_form: @coupon_form }
     UpdateOrder.call(options) do
-      on(:valid) { redirect_to cart_path, notice: 'Cart successfully updated' }
-      on(:invalid) { render :edit }
+      on(:valid) { redirect_to cart_path, notice: t('carts.edit.success_updated') }
+      on(:invalid) { failed_update }
     end
+  end
+
+  private
+  def failed_update
+    flash[:alert] = t('carts.edit.failed_updated')
+    render :edit
   end
 
 end
---
+++ spec/features/cart_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+include ActionView::Helpers::NumberHelper
+
+RSpec.feature 'Cart', :type => :feature do
+
+  let(:first_book) { create :book, price: 100.00 }
+  let(:second_book) { create :book, price: 50.00 }
+  let(:coupon) { create :coupon }
+  let(:first_item) { create :order_item, book: first_book, quantity: 2 }
+  let(:second_item) { create :order_item, book: second_book, quantity: 2 }
+  let(:order) { create :order, order_items: [first_item, second_item] }
+
+  before do
+    allow_any_instance_of(ApplicationController)
+    .to receive(:current_order).and_return(order)
+    visit cart_path
+  end
+
+  def check_price(*values)
+    values.each do |value|
+      expect(page).to have_content(number_to_currency value, locale: :eu)
+    end
+  end
+
+  scenario 'default page' do
+    expect(page).to have_content(I18n.t('carts.edit.title'))
+    expect(page).to have_content(first_book.title)
+    expect(page).to have_content(second_book.title)
+
+    check_price(first_book.price, second_book.price, first_item.sub_total,
+    second_item.sub_total, order.sub_total)
+
+    order.order_items.each do |item|
+      expect(find_field("order_order_items_attributes_#{item.id}_quantity").value)
+      .to eq item.quantity.to_s
+    end
+  end
+
+  context 'Update order item' do
+    scenario 'success update quantity' do
+      within "#edit_order_#{order.id}" do
+        fill_in ("order_order_items_attributes_2_quantity"), with: 3
+        fill_in ("order_order_items_attributes_3_quantity"), with: 1
+        click_button I18n.t('carts.edit.update_cart')
+      end
+      expect(page).to have_content(I18n.t('carts.edit.success_updated'))
+    end
+
+    scenario 'failed update quantity' do
+      within "#edit_order_#{order.id}" do
+        fill_in ("order_order_items_attributes_2_quantity"), with: 1001
+        click_button I18n.t('carts.edit.update_cart')
+      end
+      expect(page).to have_content(I18n.t('carts.edit.failed_updated'))
+    end
+  end
+
+  context 'Update coupon' do
+    scenario 'success update coupon' do
+      within "#edit_order_#{order.id}" do
+        fill_in ("order_coupon_code"), with: coupon.code
+        click_button I18n.t('carts.edit.update_cart')
+      end
+      expect(page).to have_content(I18n.t('carts.edit.success_updated'))
+      expect(page).to have_content("#{I18n.t('carts.edit.coupon')} (#{coupon.discount}%):")
+    end
+
+    scenario 'failed update coupon' do
+      within "#edit_order_#{order.id}" do
+        fill_in ("order_coupon_code"), with: 2131241324
+        click_button I18n.t('carts.edit.update_cart')
+      end
+      expect(page).to have_content(I18n.t('carts.edit.failed_updated'))
+      expect(page).to have_content('Not found this coupon')
+    end
+  end
+
+  scenario 'destroy order item' do
+    delete_link = page.first(:xpath, "//a[@href='/order_items/#{first_item.id}']")
+    delete_link.click
+    expect(delete_link['data-confirm']).to eq 'Are you sure?'
+  end
+
+end
