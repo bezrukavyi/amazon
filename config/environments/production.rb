c988ecfaf20e06b3f7465171aaa21d06477c10f7
bezrukavyi <yaroslav555@gmail.com>
2017-01-29 21:11:03 +0200

    Added mail for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,24 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "amazon_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.app_domain = 'http://amazon555.herokuapp.com'
+
+  # Email
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: config.app_domain,
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
