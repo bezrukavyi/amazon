ac82b1d9636cc6c749050e196b9c919527d526fc
bezrukavyi <yaroslav555@gmail.com>
2017-02-01 02:16:05 +0200

    Refact user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < Devise::RegistrationsController
   before_action :set_countries, only: [:edit, :update]
 
   def update
-    params[:address] ? address_update : super
+    params[:address] ? address_update : update_user
   end
 
   def destroy
@@ -17,30 +17,38 @@ class UsersController < Devise::RegistrationsController
     end
   end
 
-  protected
+  private
 
-  def update_resource(resource, resource_params)
-    type = params[:with_password] ? 'with' : 'without'
-    resource.send("update_#{type}_password", resource_params)
-  end
-
-  def account_update_params
+  def allowed_params
     params.require(:user).permit(:email, :password, :password_confirmation,
       :current_password, :first_name, :last_name)
   end
 
-  def after_update_path_for(resource)
-    edit_user_path
+  def update_user
+    type = params[:with_password] ? 'with' : 'without'
+    if current_user.send("update_#{type}_password", allowed_params)
+      bypass_sign_in current_user
+      success_update('privacy')
+    else
+      failure_update
+    end
   end
-
-  private
 
   def address_update
     address = set_address_by_params(params[:address])
     UpdateAddress.call({ addressable: current_user, addresses: [address] }) do
-      on(:valid) { redirect_back fallback_location: root_path, notice: t('devise.registrations.updated'), anchor: 'address' }
-      on(:invalid) { render :edit }
+      on(:valid) { success_update('address') }
+      on(:invalid) { failure_update }
     end
+  end
+
+  def failure_update
+    flash[:alert] = t('devise.failure.updated')
+    render :edit
+  end
+
+  def success_update(type)
+    redirect_to edit_user_path, notice: t('devise.registrations.updated'), anchor: type
   end
 
 end
