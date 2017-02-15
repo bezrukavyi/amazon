92146a4e59431ed0025b7780031fb2ebe4b5eec6
Yaroslav Bezrukavyi <yaroslav555@gmail.com>
2017-02-15 13:10:05 +0200

    Refact header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,18 @@ module ApplicationHelper
   def empty_cart_class
     'empty' if current_order.items_count.zero?
   end
+
+  def message_wrap(key)
+    content_tag :div, class: "alert alert-#{alert_class(key)}" do
+      yield if block_given?
+    end
+  end
+
+  def alert_class(key)
+    case key
+    when 'notice' then 'success'
+    when 'alert' then 'warning'
+    else key
+    end
+  end
 end
