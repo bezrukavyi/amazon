5cf302c9d3c66c90faaa5d372c08754e9c6e08b8
Yaroslav Bezrukavyi <yaroslav555@gmail.com>
2017-02-20 15:39:38 +0200

    Connect amazonAWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
+  storage :fog
 
   def cache_dir
     "#{Rails.root}/tmp/uploads"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,8 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
-
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Figaro.env.aws_access_key_id,
-    aws_secret_access_key: Figaro.env.aws_secret_access_key,
-    region: 'eu-west-1',
-    host: Figaro.env.domain_name
+    aws_secret_access_key: Figaro.env.aws_secret_access_key
   }
-  config.fog_directory  = 'directory'
-  config.fog_public     = false
-  config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
+  config.fog_directory = Figaro.env.aws_bucket
 end
